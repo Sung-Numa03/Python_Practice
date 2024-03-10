52d3b83d04fb48f9536109fe5668e417ccb731dd
Sung-Numa03 <sungnumafroko@gmail.com>
2024-03-10 23:41:57 +0000

    A function,condition and looping

--- index.py
+++ index.py
@@ -2,12 +2,12 @@
 """
 Made a variable x with the value of 5 and integer and also _rndText which is a string
 """
-x = int(5)
+
 y = float(7)
 z = str(9)
-_rndText = "Hello World!"
 
-"""
+
+
 """
 print(x)
 print(y)
@@ -15,8 +15,19 @@ print(z)
 print(_rndText) #displaying "Hello World!"
 """
 def _MyFunction():
-    Print(_rndText + "Python is fun")
-"""
-def _MyFunction():  
-    Print(_rndText + "Python is fun")
+    global x 
+    x = int(5)
+    global _rndText
+    _rndText = "Hello World!"
+    print(_rndText + " Python is fun")
 
+_MyFunction()
+
+print(_rndText )
+
+for x in "banana":
+    print(x)
+
+txt = "The best things in life are free"
+if "free" in txt:
+    print(txt)
