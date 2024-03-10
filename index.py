2c4e13836f2f80d1b77196249c133fc0bdc125ca
Sung-Numa03 <sungnumafroko@gmail.com>
2024-03-10 22:50:48 +0000

    talked about the variables

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
+
+"""
+Made a variable x with the value of 5 and integer and also _rndText which is a string
+"""
 x = 5
 _rndText = "Hello World!"
 
 print(x)
 
-print(_rndText)
+print(_rndText) #displaying "Hello World!"
