c8a1edaaef55daf26698b26218385f4c48332c16
Sung-Numa03 <sungnumafroko@gmail.com>
2024-03-10 22:54:08 +0000

    touched a bit on type casting

--- index.py
+++ index.py
@@ -2,9 +2,12 @@
 """
 Made a variable x with the value of 5 and integer and also _rndText which is a string
 """
-x = 5
+x = int(5)
+y = float(7)
+z = str(9)
 _rndText = "Hello World!"
 
 print(x)
-
+print(y)
+print(z)
 print(_rndText) #displaying "Hello World!"
