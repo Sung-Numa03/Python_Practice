a8cee7e0a5c6a954340c6759440fde5afc6344e8
Sung-Numa03 <sungnumafroko@gmail.com>
2024-03-11 00:34:45 +0000

    working on for loops, while loops and how to use esle in for loops

--- index.py
+++ index.py
@@ -31,3 +31,41 @@ for x in "banana":
 txt = "The best things in life are free"
 if "free" in txt:
     print(txt)
+
+print("===========Loops=============")
+
+print("===========For=============")
+fruits = ["Melon", "Grape", "Avocado"]
+for k in fruits:
+    print(k)
+    if "Banana" not in fruits:
+        fruits.append("Banana")
+    if k == "Banana":
+        break
+    if "Cherry" not in fruits:
+        fruits.append("Cherry")
+
+print(fruits)
+
+print("===========Else In For=============")
+for y in range(3, 50, 4):
+    print(y)
+else:
+    print("Done looping")
+
+print("===========While=============")
+
+i = 1
+n = 5
+secret_word = 6
+counter = 1
+
+while True:
+    word = input("Enter secret number: ")
+    counter = counter + 1
+    if word == secret_word:
+        print(" You're correct😊")
+        break
+    if word != secret_word and counter > 7:
+        print("you've used all your chances😂")
+        break
